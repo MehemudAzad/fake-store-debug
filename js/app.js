9acff08d00da316617ea0e2873730c56e86f4421
Rakoonball <mehemudazad2203@gmail.com>
2022-09-11 12:49:28 +0600

    first commit

--- js/app.js
+++ js/app.js
@@ -4,8 +4,9 @@ const loadProducts = (url) => {
    fetch(url)
       .then((res) => res.json())
       .then((data) => {
-         arr.push(data);
+         // arr.push(data);
          showProducts(data);
+         console.log(data)
       });
 };
 
@@ -18,14 +19,14 @@ const showProducts = (products) => {
 
    document.getElementById("all-products").innerHTML = "";
 
-   const allProducts = products.slice(0, 10).map((pd) => pd);
+   const allProducts = products.slice(0, 12).map((pd) => pd);
    for (const product of allProducts) {
       const image = product.images;
       const div = document.createElement('div');
       div.classList.add('product');
       div.innerHTML = `<div class="single-product">
       <div>
-    <img class="product-image" src=${image}></img>
+    <img class="product-image" src=${product.image}></img>
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
@@ -34,7 +35,7 @@ const showProducts = (products) => {
       <button onclick="showProductDetails(${product.id})" id="details-btn"    data-bs-toggle="modal"
       data-bs-target="#exampleModal" class="btn btn-outline-secondary mb-2 rounded-1 mt-1">Details</button>
       
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success border-0 w-100 rounded-0 bg-main py-2">Add to cart</button>
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success border-0 w-100 rounded-2 bg-main py-2">Add to cart</button>
       `;
       document.getElementById('all-products').appendChild(div);
    }
@@ -44,8 +45,8 @@ let count = 0;
 
 const addToCart = (id, price) => {
    count = count + 1;
-   updatePrice('price', value);
-
+   updatePrice('price', price);
+   updateTotal ();
    updateTaxAndCharge();
    document.getElementById('total-Products').innerText = count;
 };
@@ -67,16 +68,16 @@ const showProductDetailsInModal = (product_details) => {
 
 const getInputValue = (id) => {
    const element = document.getElementById(id).innerText;
-   const converted = parseInt(element);
+   const converted = parseFloat(element);
    return converted;
 };
 
 // main price update function
 const updatePrice = (id, value) => {
    const convertedOldPrice = getInputValue(id);
-   const convertPrice = parseInt(value);
+   const convertPrice = parseFloat(value);
    const total = convertedOldPrice + convertPrice;
-   document.getElementById(id).innerText = Math.round(total);
+   document.getElementById(id).innerText = total;
 };
 
 // set innerText function
